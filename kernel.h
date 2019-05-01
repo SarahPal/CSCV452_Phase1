7263c69d135028433118d9cfc47d1658fcc71687
SarahPal <47961885+SarahPal@users.noreply.github.com>
2019-05-01 01:05:53 -0700

    Update kernel.h

--- kernel.h
+++ kernel.h
@@ -1,11 +1,4 @@
 #define DEBUG 1
-#define RUNNING 0
-#define READY 1
-#define BLOCKED 2
-#define ZAPPED 3
-#define QUIT 4
-#define FINISHED 5
-#define TIMESLICE 80000
 
 typedef struct proc_struct proc_struct;
 
@@ -15,6 +8,8 @@ struct proc_struct {
    proc_ptr       next_proc_ptr;
    proc_ptr       child_proc_ptr;
    proc_ptr       next_sibling_ptr;
+   int            parent_pid;
+   proc_ptr       quit_child_ptr;
    char           name[MAXNAME];     /* process's name */
    char           start_arg[MAXARG]; /* args passed to process */
    context        state;             /* current context for process */
@@ -24,10 +19,11 @@ struct proc_struct {
    char          *stack;
    unsigned int   stacksize;
    int            status;         /* READY, BLOCKED, QUIT, etc. */
-   int            numChildren;   /* The number of children the process has */
-   int            return_status;
+   int            num_child;   /* The number of children the process has */
+   //int            return_status;
    int            cur_startTime;
    int            CPUTime;
+   int            quit_code;
 
    /* other fields as needed... */
 };
@@ -51,4 +47,16 @@ union psr_values {
 #define MAXPRIORITY 1
 #define SENTINELPID 1
 #define SENTINELPRIORITY LOWEST_PRIORITY
+
+#define READY   1                                                   /* starting status of processes */
+#define RUNNING 2
+#define BLOCKED 3                                                   /* status of blocked process */
+#define ZAPPED  4                                                   /* status of zapped process, not yet quit */
+#define QUIT    5
+#define FINISHED 6
+#define UNUSED -1
+#define TIMESLICE 80000
+
+#define TRUE    1
+#define FALSE   0
 //#define LOWEST_PRIORITY 0
